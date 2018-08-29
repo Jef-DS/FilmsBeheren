634d52156643fdaed67532c6323a0c4811dc6a36
VincentBott <vincentbott@gmail.com>
2018-08-29 22:17:46 +0200

    Nog een aanpassing m.b.t. de variabelen "regisseur" en "jaarUitgebracht": Als er geen waarde wordt ingevuld, krijgen deze standaard de waarde "Onbekend".

--- src/com/company/Film.java
+++ src/com/company/Film.java
@@ -11,34 +11,35 @@ public class Film {
 
     public Film(String filmTitel, String regisseur, String jaarUitgebracht) {
 
+
         this.filmTitel = filmTitel;
-        this.regisseur = regisseur;
-        this.jaarUitgebracht = jaarUitgebracht;
+
+        if (regisseur.equals(""))
+            this.regisseur = "Onbekend";
+
+        if (jaarUitgebracht.equals(""))
+            this.jaarUitgebracht = "Onbekend";
     }
 
 
     public Film(String filmTitel) {
+
         this.filmTitel = filmTitel;
     }
 
     public String getFilmTitel() {
+
         return this.filmTitel;
     }
 
 
     public String getJaarUitgebracht() {
 
-        if (this.jaarUitgebracht.equals(""))
-            return "Onbekend";
-
         return this.jaarUitgebracht;
     }
 
+
     public String getRegisseur() {
-
-
-        if (this.regisseur.equals(""))
-            return "Onbekend";
 
         return this.regisseur;
     }
